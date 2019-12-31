991552320e47f08f899f7841c077c4bb85f7ea78
Mike Hoffert <mike.hoffert@gmail.com>
2019-12-31 09:03:24 -0700

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -4,10 +4,18 @@ defmodule CG.MixProject do
   def project do
     [
       app: :cg,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.0.0",
+      elixir: "1.9.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      name: "Church Goals API",
+      source_url: "https://github.com/ChurchGoals/ChurchGoals-API",
+      homepage_url: "https://www.churchgoals.net",
+      docs: [
+        main: "CG.Application",
+        logo: "./logo.png",
+        extras: ["README.md"]
+      ],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -20,7 +28,11 @@ defmodule CG.MixProject do
   def application do
     [
       mod: {CG.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [
+        :logger,
+        :runtime_tools,
+        :ex_machina
+      ]
     ]
   end
 
@@ -39,6 +51,12 @@ defmodule CG.MixProject do
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
+      {:credo, "~> 1.1"},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_machina, "~> 2.3"},
+      {:absinthe_ecto, "~> 0.1"},
+      {:absinthe_plug, "~> 1.4"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
@@ -54,7 +72,7 @@ defmodule CG.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "coveralls", "test"]
     ]
   end
 end
